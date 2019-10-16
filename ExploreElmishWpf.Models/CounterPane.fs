4b0fe04ab1ffeb96683de5eb054428f2f44dcd6d
Bent Tranberg <bent.tranberg@gmail.com>
2019-10-16 14:28:37 +0200

    Design time model for CounterPane.

--- ExploreElmishWpf.Models/CounterPane.fs
+++ ExploreElmishWpf.Models/CounterPane.fs
@@ -39,12 +39,14 @@ module CounterPane =
 
     let bindings () : Binding<Model, Msg> list =
         [
-            "CounterValue" |> Binding.oneWay (fun m -> m.Count)
+            "Count" |> Binding.oneWay (fun m -> m.Count)
             "Increment" |> Binding.cmd Increment
             "Decrement" |> Binding.cmd Decrement
-            "StepSize" |> Binding.twoWay(
-                (fun m -> float m.StepSize),
-                int >> SetStepSize)
+            "StepSize" |> Binding.twoWay ((fun m -> float m.StepSize), int >> SetStepSize)
             "Reset" |> Binding.cmdIf (Reset, (<>) initialModel)
             "Close" |> Binding.cmd Close
         ]
+
+    let designTimeModel =
+        let model = { Count = 3; StepSize = 7 }
+        ViewModel.designInstance model (bindings ())
