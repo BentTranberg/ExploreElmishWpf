d845977034eafb033b9b4f141906ec01b9177621
Bent Tranberg <bent.tranberg@gmail.com>
2019-11-23 17:39:14 +0100

    Working hard to get MultiSelectDemo operational.

--- ExploreElmishWpf.Models/MultiSelectDemo.fs
+++ ExploreElmishWpf.Models/MultiSelectDemo.fs
@@ -9,6 +9,7 @@ module MultiSelectDemo =
     type Line =
         {
             Id: Guid
+            Marked: bool
             DisplayText: string
         }
 
@@ -18,34 +19,52 @@ module MultiSelectDemo =
         }
 
     type Msg =
-        | Dummy
+        | SetMarked of Guid * bool
 
     let init () =
         {
             Lines = [
-                { Id = Guid.NewGuid(); DisplayText = "Zero" }
-                { Id = Guid.NewGuid(); DisplayText = "One" }
-                { Id = Guid.NewGuid(); DisplayText = "Two" }
-                { Id = Guid.NewGuid(); DisplayText = "Three" }
-                { Id = Guid.NewGuid(); DisplayText = "Four" }
-                { Id = Guid.NewGuid(); DisplayText = "Five" }
-                { Id = Guid.NewGuid(); DisplayText = "Six" }
-                { Id = Guid.NewGuid(); DisplayText = "Seven" }
-                { Id = Guid.NewGuid(); DisplayText = "Eight" }
-                { Id = Guid.NewGuid(); DisplayText = "Nine" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Zero" }
+                { Id = Guid.NewGuid(); Marked = true; DisplayText = "One" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Two" }
+                { Id = Guid.NewGuid(); Marked = true; DisplayText = "Three" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Four" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Five" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Six" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Seven" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Eight" }
+                { Id = Guid.NewGuid(); Marked = false; DisplayText = "Nine" }
             ]
         }, Cmd.none
 
     let update msg m =
         match msg with
-        | Dummy ->
-            m, Cmd.none
+        | SetMarked (id, marked) ->
+            let lines =
+                m.Lines
+                |> List.map (fun line ->
+                    if line.Id = id then
+                        { line with Marked = marked }
+                    else
+                        line)
+            { m with Lines = lines }, Cmd.none
+
+    let setMarked marked (_, line) =
+        let x = 0 // just to be able to set breakpoint here
+        SetMarked (line.Id, marked)
+
+    let getMarked (_, line) =
+        line.Marked
 
     let bindings () : Binding<Model, Msg> list =
         [
             "Lines" |> Binding.subModelSeq((fun m -> m.Lines), (fun line -> line), fun () ->
                 [
                     "Id" |> Binding.oneWay (fun (_, line) -> line.Id)
+                    "Marked" |> Binding.twoWay (
+                        getMarked,
+                        setMarked
+                        )
                     "DisplayText" |> Binding.oneWay (fun (_, line) -> line.DisplayText)
                 ])
         ]
